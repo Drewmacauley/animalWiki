08ebd156fe9be29fa2eaeadc8ce29f99508bdef4
drewparker <drewjparker91@gmail.com>
2020-08-13 15:46:45 -0700

    Add alert function in js
    Co-authored-by: William DS <wdstwo@live.com>
    Co-authored-by: Andrew Macauley <macauleyandy03@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  const age = (prompt("Type 'turtle' to learn about turtles. Type 'golden eagle' to learn about golden eagles.  Type 'kangaroo' to learn about kangaroos."));
+
+  if (turtle) {
+    $("#turtle").show();
+  } else if (golden eagle) {
+    $("#goldEagle").show
+  } else (kangaroo) {
+    $("#kangaroo").show
+  }
+
+});
